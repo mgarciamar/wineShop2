e6580822a57d1797f8da908dd03a6493cb4951e7
Mónica García <monica88sm@gmail.com>
2022-05-25 16:50:07 +0200

    Añadimos nuevas clases Winery

---
+++ src/main/java/com/example/wineshop/Winery.java
@@ -0,0 +1,60 @@
+package com.example.wineshop;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Winery")
+public class Winery {
+
+    private @Id
+    @GeneratedValue Long id;
+    private String name;
+
+    public Winery(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Winery() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Winery winery = (Winery) o;
+        return Objects.equals(id, winery.id) && Objects.equals(name, winery.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Winery{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
