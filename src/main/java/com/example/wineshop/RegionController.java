3e774caf9308161dafc991f9272f05b2b03e48fa
Alejandro Garcia Villar <alejandro.garcia-villar@gft.com>
2022-05-25 20:11:34 +0200

    Creamos la interfaz RegionRepository

---
+++ src/main/java/com/example/wineshop/RegionController.java
@@ -0,0 +1,19 @@
+package com.example.wineshop;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class RegionController {
+
+
+}
